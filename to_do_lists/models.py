14286cd632b01724e5edfa47cb8a88630eb5c8be
Wendel Henrique <souowendel@gmail.com>
2026-04-27 15:10:24 -0300

    Was created a Tarefa table on Model

--- to_do_lists/models.py
+++ to_do_lists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Tarefa (models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	tarefa = models.CharField(max_length=500)
+	concluido = models.BooleanField(default=False)
 
-# Create your models here.
+	def __str__(self):
+		return self.tarefa
