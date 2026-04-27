a103d43217ecaf44f7e5fe9217e8312339279bd6
Wendel Henrique <souowendel@gmail.com>
2026-04-27 15:12:05 -0300

    Separated the view context based on auth and separated the completed and not completed tasks

--- to_do_lists/views.py
+++ to_do_lists/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from .forms import TarefaForm
+from .models import Tarefa
 
 def index(request):
-	context = {
-		'message': 'Hello, world!',
-	}
-	return render(request, 'to_do_lists/index.html', context)
+	if request.user.is_authenticated:
+		tarefasTotal = Tarefa.objects.filter(user=request.user)
+		if tarefasTotal is not None:
+			tarefasConcluidas = []
+			tarefasNaoConcluidas = []
+			for tarefa in tarefasTotal:
+				if tarefa.concluido == True:
+					tarefasConcluidas.append(tarefa)
+				else:
+					tarefasNaoConcluidas.append(tarefa)
+		else:
+			tarefasConcluidas = None
+			tarefasNaoConcluidas = None
+		form = TarefaForm()
+		context = {
+			'message': 'Hello, world!',
+			'form': form,
+		}
+		return render(request, 'to_do_lists/index.html', context)
+	else:
+		return render(request, 'to_do_lists/index.html')
